a146a44ef5f1c3b4f6a949adc774599d5afec09c
AleeWeb <aalexanderpr@gmail.com>
2019-07-09 09:55:10 -0400

    First Firebase deploy for Cloud Functions testing

--- functions/index.js
+++ functions/index.js
@@ -5,4 +5,9 @@ const functions = require('firebase-functions');
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
-// });
+// });  Request function to run via http request with exports object
+
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello, Testing 1,2,3!");
+ });
